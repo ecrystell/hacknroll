b2035f7a6e357c1fe782df28203ab971777ff990
ecrystell <yeosugar6@gmail.com>
2025-01-18 12:31:11 +0800

    cook

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/home/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/tutorial_page/', methods=["POST", "GET"])
+def tutorial_page():
+    if request.method == "POST":
+        text = request.form["text"]
+        img = request.form["img"]
+
+        if img:
+            # img to text code
+            pass
+
+        
+        return render_template('tutorial_page.html')
 app.run()
